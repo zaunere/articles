3704713d7087c9bc3bdd452fa0bcd9cce7b602d0
Hans Zaunere <zaunere@gmail.com>
2021-07-14 03:41:54 +0700

    files for https://zaunere.medium.com/2401c583c110

--- install-skel/index.js
+++ install-skel/index.js
@@ -1,7 +1,7 @@
 exports.validateTemperature = async (req, res) => {
     try {
       if (req.body.temp < 100) {
-        res.status(200).send("qqq  sxx OK");
+        res.status(200).send("Temperature OK");
       } else {
         res.status(200).send("Too hot");
       }
